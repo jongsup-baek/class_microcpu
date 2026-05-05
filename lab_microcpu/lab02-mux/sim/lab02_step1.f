b3e9c0e4da0142835d5113a6b840350c621186b8
profb-tower <jongsup.baek@ksdcsemi.com>
2026-05-05 13:08:04 +0900

    refactor: old TB/sim 파일 삭제 (#626)

--- lab_microcpu/lab02-mux/sim/lab02_step1.f
+++
@@ -1,5 +0,0 @@
-../tb_pkg_lab02.sv
-../mux2to1.sv
-../tb_mux2to1.sv
-+access+rwc
--timescale 1ns/100ps
--- lab_microcpu/lab06-mem/sim/lab06_step1.f
+++
@@ -1,6 +0,0 @@
-../cpu_pkg.sv
-../tb_pkg_lab06.sv
-../mem.sv
-../tb_mem.sv
-+access+rwc
--timescale 1ns/100ps
