bcd8ffe326422d318ca14eda41084f2e4bcd7e37
profb-tower <jongsup.baek@ksdcsemi.com>
2026-05-05 13:47:38 +0900

    refactor: lab01 Comment 구조 확정 (#626)

--- lab_microcpu/lab01-regfile/student/tb_regfile_blank.sv
+++ lab_microcpu/lab01-regfile/student/tb_regfile_blank.sv
@@ -32,26 +32,21 @@ module tb;
       .rst_n   (rst_n)
    );
 
-   // Comment #1 : task 정의
+   task reset_dut();
+      #10;
+         rst_n   = 0;
+         wr_en   = 0;
+         wr_addr = 2'd0;
+         wr_data = 16'h0000;
+         rd_addr = 2'd0;
+         rs_addr = 2'd0;
+      @(posedge clk);
+      @(posedge clk);
+         rst_n = 1;
+      @(posedge clk);
+   endtask
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   // Comment #1 : write_reg task
 
 
 
@@ -67,29 +62,33 @@ module tb;
 
    // End Comment
 
+   // Comment #3 : write_nowr task
+
+
+
+
+
+
+   // End Comment
+
+   
    initial begin
-      // Comment #2 : 리셋 검증
+      reset_dut();
 
-      // End Comment
-
-      // Comment #3 : 쓰기 검증
+      // Comment #2 : 쓰기 검증
 
 
 
 
-      // End Comment
 
-      // Comment #4 : 읽기 검증
 
 
       // End Comment
 
-      // Comment #5 : wr_en 비활성 쓰기 시도
+      // Comment #4 : wr_en 비활성 쓰기 시도
 
 
-      // End Comment
 
-      // Comment #6 : 값 유지 확인
 
       // End Comment
 
@@ -103,16 +102,16 @@ module tb;
    //////////////////////////////////////////////////////////
    //  time  rst_n  wr_en  wr_addr  wr_data  rd_addr  rs_addr  rd_data  rs_data
    //  ----  -----  -----  -------  -------  -------  -------  -------  -------
-   //     0      0      0        0     0000        0        0     0000     0000    #2
+   //     0      0      0        0     0000        0        0     0000     0000
    //   100      1      0        0     0000        0        0     0000     0000
-   //   200      1      1        0     AAAA        0        0     AAAA     AAAA    #3
+   //   200      1      1        0     AAAA        0        0     AAAA     AAAA    #2
    //   300      1      1        1     BBBB        0        0     AAAA     AAAA
    //   400      1      1        2     CCCC        0        0     AAAA     AAAA
    //   500      1      1        3     DDDD        0        0     AAAA     AAAA
-   //   600      1      0        3     DDDD        0        1     AAAA     BBBB    #4
+   //   600      1      0        3     DDDD        0        1     AAAA     BBBB
    //   700      1      0        3     DDDD        2        3     CCCC     DDDD
-   //   800      1      0        0     EEEE        2        3     AAAA     AAAA    #5
+   //   800      1      0        0     EEEE        2        3     AAAA     AAAA    #4
    //   900      1      0        1     FFFF        2        3     AAAA     AAAA
-   //  1000      1      0        1     FFFF        0        1     AAAA     BBBB    #6
+   //  1000      1      0        1     FFFF        0        1     AAAA     BBBB
    //////////////////////////////////////////////////////////
 endmodule
