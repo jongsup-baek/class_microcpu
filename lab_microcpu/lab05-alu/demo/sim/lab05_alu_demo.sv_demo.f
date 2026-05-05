4ce7539278f9c708b9d6a3cf1d93c4c2d0e5f601
profb-tower <jongsup.baek@ksdcsemi.com>
2026-05-05 10:12:05 +0900

    refactor: lab04~06 TB 재작성 (#626)

---
+++ lab_microcpu/lab05-alu/demo/sim/lab05_alu_demo.sv_demo.f
@@ -0,0 +1,13 @@
+//////////////////////////////////////////////////////////
+// KSDC Proprietary
+// Course: 반도체설계검증 언어기초
+// File  : lab05_alu_demo.sv_demo.f
+// Date  : 2026-05-05
+// Author: Jongsup Baek <jongsup.baek@ksdcsemi.com>
+//////////////////////////////////////////////////////////
+
+// Lab files
+../lab05_alu_demo.sv
+
+// Simulation Options
++access+rwc
---
+++ lab_microcpu/lab05-alu/demo/sim/lab05_demo.f
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////
+// KSDC Proprietary
+// Course: MicroCPU 설계 실무
+// File  : lab05_demo.f
+// Date  : 2026-05-05
+// Author: Jongsup Baek <jongsup.baek@ksdcsemi.com>
+//////////////////////////////////////////////////////////
+
+// Lab files
+../lab05_alu_demo.sv
+
+// Simulation Options
++access+rwc
+-timescale 1ns/100ps
