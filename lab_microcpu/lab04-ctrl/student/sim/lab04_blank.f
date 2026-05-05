ea8675563b33bf8940c31484c8db86118b4ff88d
profb-tower <jongsup.baek@ksdcsemi.com>
2026-05-05 10:13:53 +0900

    fix: lab04~06 blank.f 추가 (#626)

---
+++ lab_microcpu/lab04-ctrl/student/sim/lab04_blank.f
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////
+// KSDC Proprietary
+// Course: MicroCPU 설계 실무
+// File  : lab04_blank.f
+// Date  : 2026-05-05
+// Author: Jongsup Baek <jongsup.baek@ksdcsemi.com>
+//////////////////////////////////////////////////////////
+
+// Lab files
+../lab04_control_blank.sv
+
+// Simulation Options
++access+rwc
+-timescale 1ns/100ps
---
+++ lab_microcpu/lab05-alu/student/sim/lab05_blank.f
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////
+// KSDC Proprietary
+// Course: MicroCPU 설계 실무
+// File  : lab05_blank.f
+// Date  : 2026-05-05
+// Author: Jongsup Baek <jongsup.baek@ksdcsemi.com>
+//////////////////////////////////////////////////////////
+
+// Lab files
+../lab05_alu_blank.sv
+
+// Simulation Options
++access+rwc
+-timescale 1ns/100ps
