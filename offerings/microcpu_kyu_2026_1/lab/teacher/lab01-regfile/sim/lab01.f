91ae121e1950b18a607694697b209f9d24514409
team_design <jongsup.baek@ksdcsemi.com>
2026-06-07 12:55:51 +0900

    refactor: offerings 재빌드 (_demo+design 반영)

---
+++ offerings/microcpu_kyu_2026_1/lab/teacher/lab01-regfile/sim/lab01.f
@@ -0,0 +1,15 @@
+//////////////////////////////////////////////////////////
+// KSDC Proprietary
+// Course: MicroCPU 설계 실무
+// File  : lab01.f
+// Date  : 2026-05-05
+// Author: Jongsup Baek <jongsup.baek@ksdcsemi.com>
+//////////////////////////////////////////////////////////
+
+// Lab files
+../regfile.sv
+../tb_regfile.sv
+
+// Simulation Options
++access+rwc
+-timescale 1ns/100ps
